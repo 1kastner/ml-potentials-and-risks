7db6f4fabc1691127f532af060c0c7c21135ee43
Marvin Kastner <1kastner@users.noreply.github.com>
2019-11-03 22:49:48 +0100

    Fixe Encoding von requirements.yml, fuege environment-picker hinzu

--- internal-development/_create_requirements.ps1
+++ internal-development/_create_requirements.ps1
@@ -1,5 +1,7 @@
 conda activate ml-hype-or-hybris
 
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+
 conda env export --no-builds --from-history > requirements.yml
 
 $dirty_content = Get-Content requirements.yml
