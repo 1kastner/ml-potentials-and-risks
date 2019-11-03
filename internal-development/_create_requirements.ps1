4f0f7bf52a0bf0fe32965740126d952b4259842f
Marvin Kastner <1kastner@users.noreply.github.com>
2019-11-03 22:22:07 +0100

    Write a small internal script for dumping the requirements nicely

---
+++ internal-development/_create_requirements.ps1
@@ -0,0 +1,8 @@
+conda activate ml-hype-or-hybris
+
+conda env export --no-builds --from-history > requirements.yml
+
+$dirty_content = Get-Content requirements.yml
+$clean_content = $dirty_content[0..($dirty_content.count-3)]
+
+$clean_content > requirements.yml
