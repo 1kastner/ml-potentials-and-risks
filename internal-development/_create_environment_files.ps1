f54adcbcd26dee5c8c3e6bdfeb382007dba6a79b
Marvin Kastner <marvin.kastner@tuhh.de>
2019-11-04 09:48:32 +0100

    Fix encoding for environment file

---
+++ internal-development/_create_environment_files.ps1
@@ -0,0 +1,14 @@
+# Use correct environment
+conda activate ml-hype-or-hybris
+
+# Export minimal environment (includes prefix with hardcoded path)
+$PSDefaultParameterValues['Out-File:Encoding'] = 'ASCII'
+conda env export --no-builds --from-history > environment.yml
+
+# Cut off hardcoded path which is in the last line
+$dirty_content = Get-Content environment.yml
+$clean_content = $dirty_content[0..($dirty_content.count-3)]
+$clean_content > requirements.yml
+
+# To be on the safe side, also create an explicit file
+conda list --explicit > spec-file.txt
