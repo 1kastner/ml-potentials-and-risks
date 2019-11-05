117d6b87d2c42653770783836a3f10750f8398bb
Marvin Kastner <1kastner@users.noreply.github.com>
2019-11-05 17:39:23 +0100

    Improve updating environent descriptors

--- internal-development/_create_environment_files.ps1
+++ internal-development/_create_environment_files.ps1
@@ -1,9 +1,19 @@
+# This script updates the environment.yml and spec-file.txt
+# It is meant to be used by the developer of the repository for maintenance
+
+# Stop on first error
+Set-StrictMode -Version latest
+Set-PSDebug -Trace 1
+$ErrorActionPreference = "Stop"
+
 # Use correct environment
 conda activate ml-hype-or-hybris
+if (-not $?) {throw "Failed to activate conda environment 'ml-hype-or-hybris'"}
 
 # Export minimal environment (includes prefix with hardcoded path)
 $PSDefaultParameterValues['Out-File:Encoding'] = 'ASCII'
 conda env export --no-builds --from-history > environment.yml
+if (-not $?) {throw "Failed to export conda environment as YAML"}
 
 # Cut off hardcoded path which is in the last line
 $dirty_content = Get-Content environment.yml
@@ -12,3 +22,4 @@ $clean_content > environment.yml
 
 # To be on the safe side, also create an explicit file
 conda list --explicit > spec-file.txt
+if (-not $?) {throw "Failed to export conda environment as SPEC-File"}
