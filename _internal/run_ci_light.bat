ad7e84143d7c18700f7b8e36d78a473131965d33
Marvin Kastner <marvin.kastner@tuhh.de>
2025-01-01 16:09:36 +0100

    Add reminder to myself

--- _internal/run_ci_light.bat
+++ _internal/run_ci_light.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 ECHO.Please run this batch script as a substitute of the GitHub workflows.
-ECHO.This light version does not run the fresh conda installation.
+ECHO.This light version of the CI does not run the fresh conda installation.
 
 REM Move to project root directory
 CD %~dp0\..
@@ -12,9 +12,9 @@ flake8_nb || ECHO.While linting, flake8_nb failed && EXIT /B
 
 REM build docs
 CALL python -m sphinx -W --keep-going ./ ./.sphinx/_build/  ^
-    || ECHO.While building the docs, sphinx failed && EXIT /B
+    || ECHO.While building the docs, sphinx failed. For further information, please check <PROJECT-ROOT>/.sphinx/Readme.md. && EXIT /B
 CALL python -m sphinx -W --keep-going -b linkcheck ./ ./.sphinx/_build/ ^
-    || ECHO.While checking the links, sphinx failed && EXIT /B
+    || ECHO.While checking the links, sphinx failed. For further information, please check <PROJECT-ROOT>/.sphinx/Readme.md. && EXIT /B
 
 ECHO.All steps were executed successfully. Please consider also checking the skipped CI steps manually if you changed
 ECHO.related files.
